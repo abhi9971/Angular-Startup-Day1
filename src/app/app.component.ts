5d3c0524cc2b5cafb92875191cdfec86f1175b96
abhi9971 <abhishekrajawat101gmail.com>
2022-03-07 20:22:05 +0530

    Ability to launch bridgelabz site in a new tab on clicking bridgelabz logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Helloworld';
   imageUrl="../assets/bridgelabz-logo.png";
+  url="https://bridgelabz.com/";
 
   ngOnInit(): void{
-    this.title="Hello from bridgelabz"
+    this.title="Hello from bridgelabz";
+  }
+  onClick($event: any){
+    console.log("Save button is clicked!",$event);
+    window.open(this.url,"_blank");
   }
 }
